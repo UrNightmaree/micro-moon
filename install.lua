1986ee45b44a6a5e03c2a24588c55b99c6ff3207
UN =) <71205197+UrNightmaree@users.noreply.github.com>
2022-07-13 17:23:11 +0800

    Create install.lua

---
+++ install.lua
@@ -0,0 +1,34 @@
+#!/usr/bin/env lua
+
+function exists(file)
+   local ok, err, code = os.rename(file, file);
+   if not ok then
+      if code == 13 then
+         return true;
+      end
+   end
+   return ok, err;
+end;
+
+function isdir(path) return exists(path.."/") end;
+
+local exec = os.execute;
+
+if exec('which git') == 'not found' then
+   print 'Install git!';
+   return;
+end
+if exec('which micro') == 'not found' then
+   print 'Install Micro!';
+   return;
+end
+
+exec 'git clone https://github.com/UrNightmaree/micro-moon';
+
+if not isdir('~/.config/micro/syntax') then
+   exec 'mkdir ~/.config/micro/syntax';
+end
+
+exec 'cd micro-moon && mv moonscript.yaml ~/.config/micro/syntax';
+
+print 'Installation complete'
